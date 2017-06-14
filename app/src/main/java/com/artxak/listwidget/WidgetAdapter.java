197e11eda458b8f76afdb02b0310424207eade23
Arthur Khakimov <arthur78@cloud.upwork.com>
2017-06-14 09:36:36 +0300

    Add list widget and set list adapter

---
+++ app/src/main/java/com/artxak/listwidget/WidgetAdapter.java
@@ -0,0 +1,61 @@
+package com.artxak.listwidget;
+
+import android.content.Context;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+public class WidgetAdapter implements RemoteViewsService.RemoteViewsFactory {
+    Context mContext;
+    String[] list = {"Treehouse", "Android", "Java", "Kotlin", "Anko"};
+
+    public WidgetAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDataSetChanged() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return list.length;
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        RemoteViews remoteViews = new RemoteViews(mContext.getPackageName(), R.layout.list_item);
+        remoteViews.setTextViewText(R.id.textView, list[position]);
+        return remoteViews;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+}
